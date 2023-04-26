c9446cd82cced0ae152c17a5e8c26aed3fa2e5f5
Douglas Moreira <33124673+dougmoreira@users.noreply.github.com>
2023-04-26 11:19:57 -0300

    creating new project

---
+++ FoodDelivery/FoodDelivery/FoodDeliveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodDeliveryApp.swift
+//  FoodDelivery
+//
+//  Created by Douglas Moreira on 26/04/23.
+//
+
+import SwiftUI
+
+@main
+struct FoodDeliveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
