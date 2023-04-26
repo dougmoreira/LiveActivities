860fc076659f6d8d286d98af76b935299ecafd32
Douglas Moreira <33124673+dougmoreira@users.noreply.github.com>
2023-04-26 11:27:10 -0300

    creating widget with live activity

---
+++ FoodDelivery/LiveActivity/LiveActivityBundle.swift
@@ -0,0 +1,17 @@
+//
+//  LiveActivityBundle.swift
+//  LiveActivity
+//
+//  Created by Douglas Moreira on 26/04/23.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct LiveActivityBundle: WidgetBundle {
+    var body: some Widget {
+        LiveActivity()
+        LiveActivityLiveActivity()
+    }
+}
